ee279d8a25abf60042a5dc007302ecc508f438fa
Aliefes <dr.ali.ihsan.anadolulu@gmail.com>
2024-10-14 20:06:19 +0300

    first.py

--- first.py
+++ first.py
@@ -1,1 +1,6 @@
+import random
+gercek = ('Kitap okumayı ve bilgisayar oynamayı severim. ' , 'Kardeşimin kuşunun adı Osmandır. '  )
 
+rastgele_bilgi = random .choice(gercek)
+
+print('Benim hakkımda gerçekler:' , rastgele_bilgi)
